d55bfdfe770e3b4c3f5e36e9a6d464dd9585b14a
Masaharu Hosomichi <masaharuhosomichi@MasaharunoiMac.local>
2019-11-28 20:21:51 +0900

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/add_place_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
+      ),
+    );
+  }
+}
